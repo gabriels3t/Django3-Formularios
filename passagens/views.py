d73f41a7835c3aa313e2aac0ff7fe3f23e2b9088
gabriels3t <gabriels3t@hotmail.com>
2021-01-14 14:52:21 -0300

    Alterando e manipulando dados

--- passagens/views.py
+++ passagens/views.py
@@ -7,3 +7,12 @@ def index(request):
         'form':form
     }
     return render(request,'index.html',contexto)
+
+def revisao_consulta(request):
+    if request.method == 'POST':
+        form = PassagemForms(request.POST)
+        contexto = {
+            'form':form
+        }
+        return render(request,'minha_consulta.html',contexto)
+    
